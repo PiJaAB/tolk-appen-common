2c6ed30ce7b167f2839c97756ec000aa6438cafe
Martinlindblad <36680040+Martinlindblad@users.noreply.github.com>
2022-03-15 10:32:59 +0100

    Create job.ts

---
+++ src/utils/types/job.ts
@@ -0,0 +1,28 @@
+type InterpreterJob<T, Timestamp> = {
+  type: T;
+  title: string;
+  jobId: string;
+} & (
+  | {
+      start: Timestamp;
+      end: Timestamp;
+    }
+  | {
+      start: Timestamp | null;
+      end: Timestamp | null;
+    }
+);
+
+type TranslatorJob<T, Timestamp> = {
+  type: T;
+  title: string;
+  deadLineDate: Timestamp;
+  jobId: string;
+};
+
+export declare type G_Jobs<Timestamp> = {
+  interpreter: InterpreterJob<'interpreter', Timestamp>;
+  translator: TranslatorJob<'translator', Timestamp>;
+};
+export declare type G_Job<Timestamp> =
+  G_Jobs<Timestamp>[keyof G_Jobs<Timestamp>];
