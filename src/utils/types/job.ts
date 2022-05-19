e940bd7c8c76d6e2adebf67a76c0f137cbcabd26
markusekblad <markus.ekblad@pija.se>
2022-05-19 23:12:14 +0200

    Add new fields

--- src/utils/types/job.ts
+++ src/utils/types/job.ts
@@ -1,23 +1,34 @@
+export interface JobAddress {
+  name?: string;
+  streetAddress: string;
+  city: string;
+  country: string;
+}
+
 export interface BaseAssignmentDetail {
   mediator: string;
   assignmentNumber: string;
 }
 export interface BaseJob {
   title: string;
-  asssignmentDetails: BaseAssignmentDetail;
+  assignmentDetails: BaseAssignmentDetail;
 }
-
 export interface InterpreterAssignmentDetail extends BaseAssignmentDetail {
   administrator: string;
   customer: string;
 }
 export interface BaseJobInterpreter extends BaseJob {
-  asssignmentDetails: InterpreterAssignmentDetail;
+  assignmentDetails: InterpreterAssignmentDetail;
 }
 export interface G_InterpreterJob<Timestamp> extends BaseJobInterpreter {
   type: 'interpreter';
   start: Timestamp;
   end: Timestamp;
+  jobAddress?: JobAddress;
+  online: boolean;
+  onlineLink?: string;
+  category: 'Video' | 'Phone' | 'InSitu';
+  languages: string[];
 }
 export interface G_TranslatorJob<Timestamp> extends BaseJob {
   type: 'translator';
